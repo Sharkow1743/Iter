98c95fae830f6b50980f92af0adbe408975d5b7a
Sharkow1743 <sharkow1743@gmail.com>
2026-02-06 04:19:02 +0700

    add attachments to add_comment, add reply_to_comment

--- itd/routes/comments.py
+++ itd/routes/comments.py
@@ -1,10 +1,12 @@
 from itd.request import fetch
 
-def add_comment(token: str, post_id: str, content: str, reply_comment_id: str | None = None):
-    data = {'content': content}
-    if reply_comment_id:
-        data['replyTo'] = str(reply_comment_id)
+def add_comment(token: str, post_id: str, content: str, attachment_ids: list[str] = []):
+    data = {'content': content, "attachmentIds": attachment_ids}
     return fetch(token, 'post', f'posts/{post_id}/comments', data)
+
+def reply_to_comment(token: str, comment_id: str, content: str, attachment_ids: list[str] = []):
+    data = {'content': content, "attachmentIds": attachment_ids}
+    return fetch(token, 'post', f'comments/{comment_id}/replies', data)
 
 def get_comments(token: str, post_id: str, limit: int = 20, cursor: int = 0, sort: str = 'popular'):
     return fetch(token, 'get', f'posts/{post_id}/comments', {'limit': limit, 'sort': sort, 'cursor': cursor})
